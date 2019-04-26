43c5bd9ef0b470cc54b371c3f06a8d52f860ae0a
tomatebio <carlostomate@gmail.com>
2019-04-26 13:54:12 -0300

    ultima aula

---
+++ AulaVI/CodAula.r
@@ -0,0 +1,107 @@
+  ##  Ánalise de variancia em populacoes de ..
+      ## Fulano da Silva 
+      # 18/2/2019 
+ 
+      # Pacotes
+        require(ggplot)
+        require(MASS)
+
+      # Funcoes
+        source(funcoes.r)
+
+      # Dados de entrada
+     input<-"dados/medicoes.txt"
+
+
+
+## Funcao que faz a media e soma
+# INPUT  my_vector  um vetor de numero
+## OUTPUT um escalar com a média
+
+my_mean <- function(my_vector) {
+   sum(my_vector)/length(my_vector)
+}
+
+
+
+## Nomes sao importantes
+## 
+## evite as palavras que já sejam usadas no R : mean, diff
+## evite acentos
+## Cuidado com ambiguidades
+
+
+## Péssimos exemplos
+
+   media<-
+   medN<-
+   ppop<-
+   
+## Melhor
+## 
+   mediaDaParcela<-
+   medicaoNumerica<-
+   probabilidadePopulacinal<-
+   probPopulacional<-
+   
+   quandoComecarUmaPalavraColoqueEmMaiuscula
+    
+## Não reutiluze os nomes, adicione sufixos. Padronize!
+
+  amostra<-
+  amostra_media<-mean(amostra)
+  amostra.desvio<-sd(amostra)
+  
+
+## Use identacao no codigo 
+
+numero<-21
+aux<-numero-1
+primo<-TRUE
+while(primo==TRUE &aux >1){
+if(numero%%aux==0){
+primo<-FALSE
+
+}else{
+    
+aux=aux-1
+print(aux)
+}
+}
+print(primo)
+
+
+
+
+## Olha que mais "bunitinho"
+## 8 tabs
+numero<-21
+aux<- numero-1
+primo<-TRUE
+while( primo==TRUE &aux >1){
+        if( numero%%aux==0){
+                primo<- FALSE
+                
+        }else{
+                
+                aux= aux-1
+                print(aux)
+        }
+}
+print(primo)
+
+x<- 1
+plot(x,)
+
+
+
+## Criador automatico de pastas 
+install.packages("ProjectTemplate")
+library("ProjectTemplate")
+
+create.project("aulaCOMSOL", rstudio.project=T)
+
+
+
+## Criar um . Rproj
+## 
---
+++ AulaVI/auto/BoasPraticas.el
@@ -0,0 +1,29 @@
+(TeX-add-style-hook
+ "BoasPraticas"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "handout")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("helvet" "scaled") ("fontenc" "T1") ("babel" "brazil")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "inputenc"
+    "verbatim"
+    "tikz"
+    "pgf"
+    "helvet"
+    "fontenc"
+    "babel"))
+ :latex)
+
